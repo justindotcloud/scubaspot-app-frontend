1bf780bf49d007d2f011c23abc55a49e7c1ae970
Justin Janson <justinjanson@macbook-pro-justin.local>
2025-12-23 17:13:56 +0100

    Remove unused Amplify backend packages

--- amplify/backend.ts
+++
@@ -1,9 +0,0 @@
-import { defineBackend } from '@aws-amplify/backend';
-
-/**
- * Minimal Amplify backend definition for Next.js deployment.
- * Add auth, data, storage, or functions as needed.
- * @see https://docs.amplify.aws/react/build-a-backend/
- */
-defineBackend({});
-
